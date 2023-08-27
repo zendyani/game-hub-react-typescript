8c4006246e82b46c0b890b212c80f0ad25ba642c
Jalil Belakhdar <jalil.belakhdar@trulyexperiences.com>
2023-08-27 19:03:33 +0100

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react'
 import useGenres from '../hooks/useGenres'
+import getCropperImageUrl from '../services/image-url'
 
 const GenreList = () => {
     const { data, error, isLoading } = useGenres()
     return (
-        <div>
-            {data.map(g => <p>{g.name}</p>)}
-        </div>
+        <List>
+            {data.map(g => 
+                <ListItem paddingY='5px'>
+                    <HStack>
+                        <Image boxSize='32px' borderRadius={8} src={getCropperImageUrl(g.image_background)} />
+                        <Text fontSize={'lg'}>{g.name}</Text>
+                    </HStack>
+                </ListItem>
+            )}
+        </List>
     )
 }
 
