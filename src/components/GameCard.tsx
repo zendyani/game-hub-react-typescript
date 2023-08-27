89d99c33c2ee10486bc4959b0502863d4ff80e9c
Jalil Belakhdar <jalil.belakhdar@trulyexperiences.com>
2023-08-27 17:29:53 +0100

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
     return (
-        <Card borderRadius={10} overflow='hidden'>
+        <Card borderRadius={10} overflow='hidden' width='340px'>
             <Image src={getCropperImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,16 @@
 import { SimpleGrid } from '@chakra-ui/react'
 import useGames from '../hooks/useGames'
 import GameCard from './GameCard'
+import GameCardSkeleton from './GameCardSkeleton'
 
 const GameGrid = () => {
-    const { games, error } = useGames()
-
+    const { games, error, isLoading } = useGames()
+    const skeletons = [1, 2, 3, 4, 5, 6]
     return (
         <>
             {error && <p>{error}</p>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding={10}>
+                {isLoading && skeletons.map(s => <GameCardSkeleton key={s} />)}
                 {games.map((game) => (
                     <GameCard game={game} key={game.id} />
                 ))}
